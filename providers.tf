5ce2dd806aca68b6ea1caf2f1cb360edc077cde6
Your Name <az.okolo.2084@gmail.com>
2025-05-20 01:06:24 +0100

    created vpc

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 # configure aws provider to establish a secure connection between terraform and aws
 provider "aws" {
-  region = var.region
+  region  = var.region
   profile = "terraform-user"
 
   default_tags {
     tags = {
-      "Automation" = "terraform"
-      "project" = var.project_name
+      "Automation"  = "terraform"
+      "project"     = var.project_name
       "Environment" = var.environment
     }
   }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,56 @@
 # environment variables
 variable "region" {
   description = "region to create resources"
-  type = string
+  type        = string
 }
 
 variable "project_name" {
   description = "project name"
-  type = string
+  type        = string
 }
 
 variable "environment" {
   description = "environment"
-  type = string
+  type        = string
+}
+
+# vpc variables
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type        = string
+}
+
+# public subnet variables
+variable "public_subnet_az1" {
+  description = "public subnet az1 cidr block"
+  type        = string
+}
+
+variable "public_subnet_az2" {
+  description = "public subnet az2 cidr block"
+  type        = string
+}
+
+# private app az1 subnet variables
+variable "private_app_subnet_az1" {
+  description = "private app subnet az1 cidr block"
+  type        = string
+}
+
+# private app az2 subnet variables
+variable "private_app_subnet_az2" {
+  description = "private app subnet az2 cidr block"
+  type        = string
+}
+
+# private data az1 subnet variables
+variable "private_data_subnet_az1" {
+  description = "private data subnet az1 cidr block"
+  type        = string
+}
+
+# private data az2 subnet variables
+variable "private_data_subnet_az2" {
+  description = "private data subnet az2 cidr block"
+  type        = string
 }
